14228e5cc6a84590969ec5fc08b1676e60669efd
thainansousa <thainansousa7@gmail.com>
2020-06-26 20:14:48 -0300

    finaliação do projeto

--- js/home.js
+++ js/home.js
@@ -1,6 +1,10 @@
 const menuMobile = document.querySelector('section.container .menuMobile')
 const containerMenuMobile = document.querySelector('header .ContainerMenuMobile')
 
+function msg(){
+    alert('Opa, bem vindo! Passando aqui só para ressaltar que esse não é o site oficial da Nubank, ok? O oficial é o nubank.com.br ;)')
+}
+
 menuMobile.addEventListener('click', () => {
     containerMenuMobile.classList.toggle('on')
     menuMobile.classList.toggle('on')
